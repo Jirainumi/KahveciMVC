09076134ed675f23294f80d621773569b0d496eb
Eren Cem Güç <erencemguc@gmail.com>
2019-03-08 01:27:14 +0300

    Wishlist View ı eklendi(Controllerdaki yorum paragrafına bakarsınız)

---
+++ CoffeLand/CoffeeLand_UI/Controllers/WishListController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeLand_UI.Controllers
+{
+    public class WishListController : Controller
+    {
+        /*
+         İstek listesini sepetten farklı olarak bilerek yazdım daha ayrı clean bi şekilde kodlarız İsteğe bağlı tek Controller a atabiliriz
+             Database e hem WishList Hemde Cart tabloları eklenicek
+             Şuan içinde örnek 2 tane öylesine orjinal templatedeki ürün var
+             */
+        public ActionResult WishList()
+        {
+            return View();
+        }
+    }
+}
