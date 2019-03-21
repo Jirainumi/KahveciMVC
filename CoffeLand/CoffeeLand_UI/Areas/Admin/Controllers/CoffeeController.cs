396181a7e57cae9f0fdf44ab3a8d85bb5689c10e
Ömer Aydın <omeraydin133@gmail.com>
2019-03-22 02:09:50 +0300

    Admin Panelinde Coffee deki İmage için FileUpload entegre edildi. Admin panelinde Admin-Boss Yetki ayrımı yapıldı. images klasörü düzenlendi.

--- CoffeLand/CoffeeLand_UI/Areas/Admin/Controllers/CoffeeController.cs
+++ CoffeLand/CoffeeLand_UI/Areas/Admin/Controllers/CoffeeController.cs
@@ -14,75 +14,75 @@ namespace CoffeeLand_UI.Areas.Admin.Controllers
 {
     public class CoffeeController : Controller
     {
-		CoffeeConcrete _coffeeConcrete;
-		CategoryConcrete _categoryConcrete;
+        CoffeeConcrete _coffeeConcrete;
+        CategoryConcrete _categoryConcrete;
 
-		public CoffeeController()
-		{
-			_coffeeConcrete = new CoffeeConcrete();
-			_categoryConcrete = new CategoryConcrete();
-		}
-
-		// GET: Admin/Coffee
-		public ActionResult Index()
+        public CoffeeController()
         {
-			Customer customer = Session["OnlineKullanici"] as Customer;
+            _coffeeConcrete = new CoffeeConcrete();
+            _categoryConcrete = new CategoryConcrete();
+        }
 
-			if (customer == null)
-			{
-				return Redirect("/Login/Login");
-			}
-			else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
-			{
-				return View(_coffeeConcrete._coffeeRepository.GetAll());
-			}
-			else
-			{
-				return Redirect("/Coffee/Coffees");
-			}
+        // GET: Admin/Coffee
+        public ActionResult Index()
+        {
+            Customer customer = Session["OnlineKullanici"] as Customer;
+
+            if (customer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
+            {
+                return View(_coffeeConcrete._coffeeRepository.GetAll());
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
         }
 
         // GET: Admin/Coffee/Details/5
         public ActionResult Details(int id)
         {
-			Customer customer = Session["OnlineKullanici"] as Customer;
+            Customer customer = Session["OnlineKullanici"] as Customer;
 
-			if (customer == null)
-			{
-				return Redirect("/Login/Login");
-			}
-			else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
-			{
-				Coffee coffee = _coffeeConcrete._coffeeRepository.GetById(id);
-				return View(coffee);
-			}
-			else
-			{
-				return Redirect("/Coffee/Coffees");
-			}
-			
+            if (customer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
+            {
+                Coffee coffee = _coffeeConcrete._coffeeRepository.GetById(id);
+                return View(coffee);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+
         }
 
         // GET: Admin/Coffee/Create
         public ActionResult Create()
         {
-			Customer customer = Session["OnlineKullanici"] as Customer;
+            Customer customer = Session["OnlineKullanici"] as Customer;
 
-			if (customer == null)
-			{
-				return Redirect("/Login/Login");
-			}
-			else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
-			{
-				ViewBag.CategoryID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "CategoryName");
-				ViewBag.ExtraMaterialsID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "Name");
-				return View();
-			}
-			else
-			{
-				return Redirect("/Coffee/Coffees");
-			}
-			
+            if (customer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
+            {
+                ViewBag.CategoryID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "CategoryName");
+                ViewBag.ExtraMaterialsID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "Name");
+                return View();
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+
         }
 
         // POST: Admin/Coffee/Create
@@ -90,54 +90,66 @@ namespace CoffeeLand_UI.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,CoffeeName,Description,Price,AVGPoint,ImageUrl,AltText,IsPrepared,ExtraMaterialsID,CategoryID")] Coffee coffee)
+        public ActionResult Create([Bind(Include = "ID,CoffeeName,Description,Price,AVGPoint,ImageUrl,AltText,IsPrepared,ExtraMaterialsID,CategoryID")] Coffee coffee, HttpPostedFileBase file)
         {
-			Customer customer = Session["OnlineKullanici"] as Customer;
+            Customer customer = Session["OnlineKullanici"] as Customer;
 
-			if (customer == null)
-			{
-				return Redirect("/Login/Login");
-			}
-			else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
-			{
-				if (ModelState.IsValid)
-				{
-					_coffeeConcrete._coffeeRepository.Insert(coffee);
-					_coffeeConcrete._coffeeUnitOfWork.SaveChanges();
-					return RedirectToAction("Index");
-				}
+            if (customer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
+            {
+                if (ModelState.IsValid)
+                {
+                    if (file != null)
+                    {
+                        file.SaveAs(HttpContext.Server.MapPath("~/images/urunler/") + file.FileName);
+                        string path = HttpContext.Server.MapPath("~/images/urunler/") + file.FileName;
 
-				ViewBag.CategoryID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "CategoryName", coffee.CategoryID);
-				return View(coffee);
-			}
-			else
-			{
-				return Redirect("/Coffee/Coffees");
-			}
-			
+                        int baslangic = path.IndexOf(@"\images");
+                        int uzunluk = path.Length - baslangic; 
+                        path = path.Substring(baslangic, uzunluk);
+
+                        coffee.ImageUrl = path;
+                    }
+
+                    _coffeeConcrete._coffeeRepository.Insert(coffee);
+                    _coffeeConcrete._coffeeUnitOfWork.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.CategoryID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "CategoryName", coffee.CategoryID);
+                return View(coffee);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+
         }
 
         // GET: Admin/Coffee/Edit/5
         public ActionResult Edit(int id)
         {
-			Customer customer = Session["OnlineKullanici"] as Customer;
+            Customer customer = Session["OnlineKullanici"] as Customer;
 
-			if (customer == null)
-			{
-				return Redirect("/Login/Login");
-			}
-			else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
-			{
-				Coffee coffee = _coffeeConcrete._coffeeRepository.GetById(id);
+            if (customer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
+            {
+                Coffee coffee = _coffeeConcrete._coffeeRepository.GetById(id);
 
-				ViewBag.CategoryID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "CategoryName", coffee.CategoryID);
-				return View(coffee);
-			}
-			else
-			{
-				return Redirect("/Coffee/Coffees");
-			}
-			
+                ViewBag.CategoryID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "CategoryName", coffee.CategoryID);
+                return View(coffee);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+
         }
 
         // POST: Admin/Coffee/Edit/5
@@ -145,49 +157,61 @@ namespace CoffeeLand_UI.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,CoffeeName,Description,Price,AVGPoint,ImageUrl,AltText,IsPrepared,ExtraMaterialsID,CategoryID")] Coffee coffee)
+        public ActionResult Edit([Bind(Include = "ID,CoffeeName,Description,Price,AVGPoint,ImageUrl,AltText,IsPrepared,ExtraMaterialsID,CategoryID")] Coffee coffee, HttpPostedFileBase file)
         {
-			Customer customer = Session["OnlineKullanici"] as Customer;
+            Customer customer = Session["OnlineKullanici"] as Customer;
 
-			if (customer == null)
-			{
-				return Redirect("/Login/Login");
-			}
-			else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
-			{
-				if (ModelState.IsValid)
-				{
-					_coffeeConcrete._coffeeRepository.Update(coffee);
-					_coffeeConcrete._coffeeUnitOfWork.SaveChanges();
-					return RedirectToAction("Index");
-				}
-				ViewBag.CategoryID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "CategoryName", coffee.CategoryID);
-				return View(coffee);
-			}
-			else
-			{
-				return Redirect("/Coffee/Coffees");
-			}			
+            if (customer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
+            {
+                if (ModelState.IsValid)
+                {
+                    if (file != null)
+                    {
+                        file.SaveAs(HttpContext.Server.MapPath("~/images/urunler/") + file.FileName);
+                        string path = HttpContext.Server.MapPath("~/images/urunler/") + file.FileName;
+
+                        int baslangic = path.IndexOf(@"\images");
+                        int uzunluk = path.Length - baslangic;
+                        path = path.Substring(baslangic, uzunluk);
+
+                        coffee.ImageUrl = path;
+                    }
+
+                    _coffeeConcrete._coffeeRepository.Update(coffee);
+                    _coffeeConcrete._coffeeUnitOfWork.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                ViewBag.CategoryID = new SelectList(_categoryConcrete._categoryRepository.GetEntity(), "ID", "CategoryName", coffee.CategoryID);
+                return View(coffee);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
         }
 
         // GET: Admin/Coffee/Delete/5
         public ActionResult Delete(int id)
         {
-			Customer customer = Session["OnlineKullanici"] as Customer;
+            Customer customer = Session["OnlineKullanici"] as Customer;
 
-			if (customer == null)
-			{
-				return Redirect("/Login/Login");
-			}
-			else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
-			{
-				Coffee coffee = _coffeeConcrete._coffeeRepository.GetById(id);
-				return View(coffee);
-			}
-			else
-			{
-				return Redirect("/Coffee/Coffees");
-			}			
+            if (customer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
+            {
+                Coffee coffee = _coffeeConcrete._coffeeRepository.GetById(id);
+                return View(coffee);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
         }
 
         // POST: Admin/Coffee/Delete/5
@@ -195,31 +219,31 @@ namespace CoffeeLand_UI.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-			Customer customer = Session["OnlineKullanici"] as Customer;
+            Customer customer = Session["OnlineKullanici"] as Customer;
 
-			if (customer == null)
-			{
-				return Redirect("/Login/Login");
-			}
-			else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
-			{
-				Coffee coffee = _coffeeConcrete._coffeeRepository.GetById(id);
-				_coffeeConcrete._coffeeRepository.Delete(coffee);
-				_coffeeConcrete._coffeeUnitOfWork.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			else
-			{
-				return Redirect("/Coffee/Coffees");
-			}
-			
+            if (customer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (customer.AuthorizationID == 1 || customer.AuthorizationID == 2)
+            {
+                Coffee coffee = _coffeeConcrete._coffeeRepository.GetById(id);
+                _coffeeConcrete._coffeeRepository.Delete(coffee);
+                _coffeeConcrete._coffeeUnitOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-				_coffeeConcrete._coffeeUnitOfWork.Dispose();
+                _coffeeConcrete._coffeeUnitOfWork.Dispose();
             }
             base.Dispose(disposing);
         }
---
+++ CoffeLand/CoffeeLand_UI/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeLand_BLL.Repository.Concrete;
+using CoffeeLand_DAL;
+using CoffeeLand_DATA.Classes;
+
+namespace CoffeeLand_UI.Areas.Admin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerConcrete _customerConcrete;
+        AuthorizationConrete _authorizationConrete;
+
+        public CustomerController()
+        {
+            _customerConcrete = new CustomerConcrete();
+            _authorizationConrete = new AuthorizationConrete();
+        }
+
+        // GET: Admin/Customer
+        public ActionResult Index()
+        {
+            Customer loggedCustomer = Session["OnlineKullanici"] as Customer;
+
+            if (loggedCustomer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (loggedCustomer.AuthorizationID == 1 || loggedCustomer.AuthorizationID == 2)
+            {
+                return View(_customerConcrete._customerRepository.GetAll());
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+        }
+
+        // GET: Admin/Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            Customer loggedCustomer = Session["OnlineKullanici"] as Customer;
+
+            if (loggedCustomer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (loggedCustomer.AuthorizationID == 1 || loggedCustomer.AuthorizationID == 2)
+            {
+                Customer customer = _customerConcrete._customerRepository.GetById(id);
+                return View(customer);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+        }
+
+        // GET: Admin/Customer/Create
+        public ActionResult Create()
+        {
+            Customer loggedCustomer = Session["OnlineKullanici"] as Customer;
+
+            if (loggedCustomer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (loggedCustomer.AuthorizationID == 1)
+            {
+                ViewBag.AuthorizationID = new SelectList(_authorizationConrete._authorizationRepository.GetEntity(), "ID", "AuthorizationName");
+                return View();
+            }
+            else if (loggedCustomer.AuthorizationID == 2)
+            {
+                ViewBag.AuthorizationID = new SelectList(_authorizationConrete._authorizationRepository.GetEntity().Where(x => x.ID == 3), "ID", "AuthorizationName");
+                return View();
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+        }
+
+        // POST: Admin/Customer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,Gender,BirthDate,UserName,Password,AuthorizationID")] Customer customer)
+        {
+            Customer loggedCustomer = Session["OnlineKullanici"] as Customer;
+
+            if (loggedCustomer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (loggedCustomer.AuthorizationID == 1)
+            {
+                if (ModelState.IsValid)
+                {
+                    _customerConcrete._customerRepository.Insert(customer);
+                    _customerConcrete._customerUnitOfWork.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.AuthorizationID = new SelectList(_authorizationConrete._authorizationRepository.GetEntity(), "ID", "AuthorizationName");
+                return View(customer);
+            }
+            else if (loggedCustomer.AuthorizationID == 2)
+            {
+                if (ModelState.IsValid)
+                {
+                    _customerConcrete._customerRepository.Insert(customer);
+                    _customerConcrete._customerUnitOfWork.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.AuthorizationID = new SelectList(_authorizationConrete._authorizationRepository.GetEntity().Where(x => x.ID == 3), "ID", "AuthorizationName");
+                return View(customer);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+        }
+
+        // GET: Admin/Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Customer loggedCustomer = Session["OnlineKullanici"] as Customer;
+
+            if (loggedCustomer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (loggedCustomer.AuthorizationID == 1)
+            {
+                Customer customer = _customerConcrete._customerRepository.GetById(id);
+
+                ViewBag.AuthorizationID = new SelectList(_authorizationConrete._authorizationRepository.GetEntity(), "ID", "AuthorizationName");
+                return View(customer);
+            }
+            else if (loggedCustomer.AuthorizationID == 2)
+            {
+                Customer customer = _customerConcrete._customerRepository.GetById(id);
+
+                ViewBag.AuthorizationID = new SelectList(_authorizationConrete._authorizationRepository.GetEntity().Where(x => x.ID == 3), "ID", "AuthorizationName");
+                return View(customer);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+        }
+
+        // POST: Admin/Customer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Gender,BirthDate,UserName,Password,AuthorizationID")] Customer customer)
+        {
+            Customer loggedCustomer = Session["OnlineKullanici"] as Customer;
+
+            if (loggedCustomer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (loggedCustomer.AuthorizationID == 1)
+            {
+                if (ModelState.IsValid)
+                {
+                    _customerConcrete._customerRepository.Update(customer);
+                    _customerConcrete._customerUnitOfWork.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.AuthorizationID = new SelectList(_authorizationConrete._authorizationRepository.GetEntity(), "ID", "AuthorizationName");
+                return View(customer);
+            }
+            else if (loggedCustomer.AuthorizationID == 2)
+            {
+                if (ModelState.IsValid)
+                {
+                    _customerConcrete._customerRepository.Update(customer);
+                    _customerConcrete._customerUnitOfWork.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                ViewBag.AuthorizationID = new SelectList(_authorizationConrete._authorizationRepository.GetEntity().Where(x => x.ID == 3), "ID", "AuthorizationName");
+                return View(customer);
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+        }
+
+        // GET: Admin/Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _customerConcrete._customerRepository.GetById(id);
+            return View(customer);
+        }
+
+        // POST: Admin/Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer loggedCustomer = Session["OnlineKullanici"] as Customer;
+
+            if (loggedCustomer == null)
+            {
+                return Redirect("/Login/Login");
+            }
+            else if (loggedCustomer.AuthorizationID == 1 || loggedCustomer.AuthorizationID == 2)
+            {
+                Customer customer = _customerConcrete._customerRepository.GetById(id);
+                _customerConcrete._customerRepository.Delete(customer);
+                _customerConcrete._customerUnitOfWork.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return Redirect("/Coffee/Coffees");
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _customerConcrete._customerUnitOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
